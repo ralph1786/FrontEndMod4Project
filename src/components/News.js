d318a8e9d58d416fb00e800dfc208fa73f2343c4
ralph1786 <ralph1786@gmail.com>
2019-04-03 12:26:45 -0400

    Moved inline styling to css new css file

--- src/components/News.js
+++ src/components/News.js
@@ -1,32 +1,11 @@
 import React from "react";
-
-// -webkit - box - shadow: 10px 10px 5px 0px rgba(128, 123, 128, 1);
-// -moz - box - shadow: 10px 10px 5px 0px rgba(128, 123, 128, 1);
-// box - shadow: 10px 10px 5px 0px rgba(128, 123, 128, 1);
-
-const styles = {
-  WebkitBoxShadow: "10px 10px 5px -5px rgba(181,174,181,1)",
-  MozBoxShadow: "10px 10px 5px -5px rgba(181,174,181,1)",
-  boxShadow: "10px 10px 5px -5px rgba(181,174,181,1)",
-  margin: "10px",
-  backgroundColor: "#faf8f5",
-  padding: "10px",
-  borderRadius: "10px",
-  fontSize: "1.3em"
-};
-
-const linkStyles = {
-  color: "black",
-  textDecoration: "none"
-};
+import "./News.css";
 
 const News = props => {
-  // console.log(props.article);
-
   const { title, url } = props.article;
   return (
-    <div style={styles}>
-      <a href={url} style={linkStyles}>
+    <div className="News">
+      <a href={url}>
         <span>{title}</span>
       </a>
     </div>
