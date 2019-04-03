ef1ba2c140f7dcdd7aad7da8698abef1f56b4ec6
ralph1786 <ralph1786@gmail.com>
2019-04-03 12:53:05 -0400

    moved inline styling to new css file

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,48 +1,20 @@
 import React from "react";
-
-const styles = {
-  width: "130px",
-  height: "125px",
-  margin: "5px",
-  display: "inline-block",
-  WebkitBoxShadow: "10px 10px 5px -5px rgba(181,174,181,1)",
-  MozBoxShadow: "10px 10px 5px -5px rgba(181,174,181,1)",
-  boxShadow: "10px 10px 5px -5px rgba(181,174,181,1)",
-  backgroundColor: "#CF5300",
-  borderRadius: "50%"
-};
-
-const spanStyles = {
-  marginLeft: "11px",
-  fontSize: "1.3rem"
-};
-
-const pTagStyles = {
-  margin: "15px",
-  fontSize: "1.3rem",
-  color: "white"
-};
-
-const clockStyles = {
-  marginTop: "-10px",
-  marginLeft: "30px",
-  color: "black"
-};
+import "./Scores.css";
 
 const Scores = props => {
   // console.log(props.info);
   const { hTeam, vTeam, clock, startTimeEastern } = props.info;
   return (
-    <div style={styles}>
-      <p style={pTagStyles}>
+    <div className="Scores">
+      <p>
         {vTeam.triCode}
-        <span style={spanStyles}>{vTeam.score}</span>
+        <span>{vTeam.score}</span>
       </p>
-      <p style={pTagStyles}>
+      <p>
         {hTeam.triCode}
-        <span style={spanStyles}>{hTeam.score}</span>
+        <span>{hTeam.score}</span>
       </p>
-      <h5 style={clockStyles}>{clock ? clock : startTimeEastern}</h5>
+      <h5>{clock ? clock : startTimeEastern}</h5>
     </div>
   );
 };
