7a5351a7f81185eba6e9b26eeef8ffbcb94e88c6
ralph1786 <ralph1786@gmail.com>
2019-04-04 11:35:09 -0400

    new css file and feature to check score day before

--- src/containers/ScoreContainer.js
+++ src/containers/ScoreContainer.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
 import Scores from "../components/Scores";
+import Modal from "react-modal";
 import axios from "axios";
 import moment from "moment";
+import "./ScoreContainer.css";
+import ModalContent from "../components/ModalContent";
 
 class ScoreContainer extends Component {
   state = {
     leagueScores: [],
-    date: moment().format("YYYYMMDD")
+    count: 0,
+    selectedGame: {},
+    showModal: false
   };
 
-  componentDidMount() {
+  fetchData = () => {
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/http://data.nba.net/10s//prod/v1/${
-          this.state.date
-        }/scoreboard.json`
+        `https://cors-anywhere.herokuapp.com/http://data.nba.net/10s//prod/v1/${moment()
+          .subtract(this.state.count, "days")
+          .format("YYYYMMDD")}/scoreboard.json`
       )
       .then(res => {
         let games = res.data.games;
@@ -22,28 +27,91 @@ class ScoreContainer extends Component {
           leagueScores: games
         });
       });
+  };
+
+  componentDidMount() {
+    this.fetchData();
+    Modal.setAppElement("body");
+  }
+
+  previousDay = () => {
+    this.setState(prevState => {
+      return { count: prevState.count + 1 };
+    });
+  };
+
+  nextDay = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  selectedGameHandler = gameObj => {
+    this.setState({
+      selectedGame: gameObj,
+      showModal: true
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      showModal: false
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      this.fetchData();
+    }
   }
 
   render() {
-    console.log("date", this.state.date);
+    console.log(this.state.selectedGame);
     let gameScores = [...this.state.leagueScores].map(game => (
-      <Scores key={game.gameId} info={game} />
+      <Scores
+        key={game.gameId}
+        info={game}
+        selectedGameHandler={this.selectedGameHandler}
+      />
     ));
 
     return (
-      <div>
-        <h2 style={styles}>Scores</h2>
-        {gameScores}
-      </div>
+      <React.Fragment>
+        <div>
+          <h2 className="Score-headline">Scores</h2>
+          <span className="prev-button" onClick={this.previousDay}>
+            <i className="fas fa-angle-double-left fa-2x" />
+          </span>
+          {this.state.count > 0 ? (
+            <span className="next-button" onClick={this.nextDay}>
+              <i className="fas fa-angle-double-right fa-2x" />
+            </span>
+          ) : null}
+          {gameScores}
+        </div>
+        <div>
+          <Modal
+            isOpen={this.state.showModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+          >
+            <ModalContent selectedGame={this.state.selectedGame} />
+          </Modal>
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-const styles = {
-  textAlign: "center",
-  color: "white",
-  fontFamily: "Impact",
-  fontSize: "2.3em"
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
 };
 
 export default ScoreContainer;
