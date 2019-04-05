08f55560123632f485c2a2b99427f402e1f22766
ralph1786 <ralph1786@gmail.com>
2019-04-05 12:42:27 -0400

    changed headers constant

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const API_ROOT = "http://localhost:3000/teams";
 export const API_WS_ROOT = "ws://localhost:3000/cable";
 export const HEADERS = {
-  "Content-Type": "application/json"
+  "Content-Type": "application/json",
+  Accept: "application/json"
 };
