7b722c468da0a2e0152f9e5787301fafef0e87ab
ralph1786 <ralph1786@gmail.com>
2019-04-22 16:34:27 -0400

    removed inline css

--- src/components/NewMessageForm.js
+++ src/components/NewMessageForm.js
@@ -1,30 +1,5 @@
 import React from "react";
-
-const stylesInput = {
-  position: "fixed",
-  bottom: "20px",
-  width: "48%",
-  height: "7%",
-  fontSize: "1.4em",
-  borderRadius: "25px",
-  outline: "none",
-  marginLeft: "23px",
-  textAlign: "center"
-};
-
-const stylesButton = {
-  position: "fixed",
-  right: "41.5%",
-  bottom: "20px",
-  width: "7%",
-  height: "7%",
-  fontSize: "1.4em",
-  backgroundColor: "#007AFF",
-  color: "white",
-  borderRadius: "50px",
-  marginRight: "23px",
-  outline: "none"
-};
+import "./NewMessageForm.css";
 
 const NewMessageForm = props => {
   return (
@@ -32,14 +7,14 @@ const NewMessageForm = props => {
       <form onSubmit={event => props.onSubmitHandler(event)}>
         {/* <label for="text">For {props.activeTeam.name}</label> */}
         <input
-          style={stylesInput}
+          className="input-style"
           onChange={event => props.onChangeHandler(event)}
           value={props.value}
           name="text"
           type="text"
           placeholder="Enter Message Here!"
         />
-        <button style={stylesButton}>
+        <button className="button-style">
           <i className="fas fa-arrow-circle-up fa-2x" />
         </button>
       </form>
